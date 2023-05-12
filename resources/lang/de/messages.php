7f1fb03d7caddf4d777fa7abfbda4015c47d432e
Simon Sedlatschek <s.sedlatschek@telekom.de>
2023-05-12 10:48:07 +0200

    fix translation

--- resources/lang/de/messages.php
+++ resources/lang/de/messages.php
@@ -9,7 +9,7 @@ return [
     'not_equals' => ':attribute darf nicht den Wert :value haben',
     'if' => 'wenn :parameter den Wert :value hat',
     'if_not' => 'wenn :parameter einen anderen Wert als :value hat',
-    'if_any_of' => 'wenn einer der Felder :parameters den Wert :value hat',
+    'if_any_of' => 'wenn eines der Felder :parameters den Wert :value hat',
     'if_all_of' => 'wenn alle der Felder :parameters den Wert :value haben',
     'if_none_of' => 'wenn keines der Felder :parameters den Wert :value hat',
 ];
